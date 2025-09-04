33fdb672c708fbb6a02930fc8197010d19ee4e37
Daazed McFarland <daazedjmcfarland@gmail.com>
2025-09-04 00:25:39 +0000

    change to shfmt so pacman can manage, underline diag

--- lua/plugins/tiny_inline_diagnostics.lua
+++ lua/plugins/tiny_inline_diagnostics.lua
@@ -1,12 +1,13 @@
 return {
-	"rachartier/tiny-inline-diagnostic.nvim",
-	event = "VeryLazy", -- Or `LspAttach`
-	priority = 1000, -- needs to be loaded in first
-	config = function()
-		require("tiny-inline-diagnostic").setup()
-		vim.diagnostic.config({
-			virtual_text = false,
-			update_in_insert = true,
-		}) -- Only if needed in your configuration, if you already have native LSP diagnostics
-	end,
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "VeryLazy", -- Or `LspAttach`
+  priority = 1000,    -- needs to be loaded in first
+  config = function()
+    require("tiny-inline-diagnostic").setup()
+    vim.diagnostic.config({
+      virtual_text = false,
+      update_in_insert = true,
+      underline = true,
+    }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+  end,
 }
