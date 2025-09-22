67a9b343cc2667d84efb18985e882176adaf6917
Daazed McFarland <daazedjmcfarland@gmail.com>
2025-09-22 07:31:03 -0700

    add shellcheck to mason ensure

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -14,7 +14,7 @@ return {
 			local lspconfig = require("lspconfig")
 
 			mason_lspconfig.setup({
-				ensure_installed = { "lua_ls", "bashls", "lua_ls", "systemd_ls" },
+				ensure_installed = { "lua_ls", "bashls", "shellcheck", "systemd_ls" },
 				automatic_installation = true,
 			})
 		end,
