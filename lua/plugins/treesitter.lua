c087343a0c104f97e7f4c13d1fc76fcc1f0794c0
Daazed J McFarland <daazedjmcfarland@gmail.com>
2026-03-06 18:24:14 -0800

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,171 +1,140 @@
--- modified version of code from this config
---https://github.com/fredrikaverpil/dotfiles/blob/main/nvim-fredrik/lua/fredrik/plugins/core/treesitter.lua
+---@module "lazy"
+---@type LazySpec
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    event = { "BufReadPost", "BufNewFile" },
-    branch = "main",
-    build = ":TSUpdate",
-    ---@class TSConfig
-    opts = {
-      -- custom handling of parsers
-      ensure_installed = {
-        "astro",
-        "bash",
-        "c",
-        "css",
-        "diff",
-        "go",
-        "gomod",
-        "gowork",
-        "gosum",
-        "graphql",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "json5",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
-        "ruby",
-      },
-    },
-    config = function(_, opts)
-      -- install parsers from custom opts.ensure_installed
-      if opts.ensure_installed and #opts.ensure_installed > 0 then
-        require("nvim-treesitter").install(opts.ensure_installed)
-        -- register and start parsers for filetypes
-        for _, parser in ipairs(opts.ensure_installed) do
-          local filetypes = parser -- In this case, parser is the filetype/language name
-          vim.treesitter.language.register(parser, filetypes)
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		{
+			"nvim-treesitter/nvim-treesitter-context",
+			opts = {
+				max_lines = 4,
+				multiline_threshold = 2,
+			},
+		},
+	},
+	lazy = false,
+	branch = "main",
+	build = ":TSUpdate",
+	config = function()
+		local ts = require("nvim-treesitter")
 
-          vim.api.nvim_create_autocmd({ "FileType" }, {
-            pattern = filetypes,
-            callback = function(event)
-              vim.treesitter.start(event.buf, parser)
-            end,
-          })
-        end
-      end
+		-- State tracking for async parser loading
+		local parsers_loaded = {}
+		local parsers_pending = {}
+		local parsers_failed = {}
 
-      -- Auto-install and start parsers for any buffer
-      vim.api.nvim_create_autocmd({ "BufRead" }, {
-        callback = function(event)
-          local bufnr = event.buf
-          local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+		local ns = vim.api.nvim_create_namespace("treesitter.async")
 
-          -- Skip if no filetype
-          if filetype == "" then
-            return
-          end
+		-- Helper to start highlighting and indentation
+		local function start(buf, lang)
+			local ok = pcall(vim.treesitter.start, buf, lang)
+			if ok then
+				vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end
+			return ok
+		end
 
-          -- Check if this filetype is already handled by explicit opts.ensure_installed config
-          for _, filetypes in pairs(opts.ensure_installed) do
-            local ft_table = type(filetypes) == "table" and filetypes or { filetypes }
-            if vim.tbl_contains(ft_table, filetype) then
-              return -- Already handled above
-            end
-          end
+		-- Install core parsers after lazy.nvim finishes loading all plugins
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LazyDone",
+			once = true,
+			callback = function()
+				ts.install({
+					"bash",
+					"comment",
+					"css",
+					"dart",
+					"diff",
+					"fish",
+					"git_config",
+					"git_rebase",
+					"gitcommit",
+					"gitignore",
+					"html",
+					"javascript",
+					"json",
+					"latex",
+					"lua",
+					"luadoc",
+					"make",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"query",
+					"regex",
+					"scss",
+					"svelte",
+					"toml",
+					"tsx",
+					"typescript",
+					"typst",
+					"vim",
+					"vimdoc",
+					"vue",
+					"xml",
+					"yaml",
+				}, {
+					max_jobs = 8,
+				})
+			end,
+		})
 
-          -- Get parser name based on filetype
-          local parser_name = vim.treesitter.language.get_lang(filetype) -- might return filetype (not helpful)
-          if not parser_name then
-            return
-          end
-          -- Try to get existing parser (helpful check if filetype was returned above)
-          local parser_configs = require("nvim-treesitter.parsers")
-          if not parser_configs[parser_name] then
-            return -- Parser not available, skip silently
-          end
+		-- Decoration provider for async parser loading
+		vim.api.nvim_set_decoration_provider(ns, {
+			on_start = vim.schedule_wrap(function()
+				if #parsers_pending == 0 then
+					return false
+				end
+				for _, data in ipairs(parsers_pending) do
+					if vim.api.nvim_buf_is_valid(data.buf) then
+						if start(data.buf, data.lang) then
+							parsers_loaded[data.lang] = true
+						else
+							parsers_failed[data.lang] = true
+						end
+					end
+				end
+				parsers_pending = {}
+			end),
+		})
 
-          local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+		local group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
 
-          if not parser_installed then
-            -- If not installed, install parser synchronously
-            require("nvim-treesitter").install({ parser_name }):wait(30000)
-          end
+		local ignore_filetypes = {
+			"checkhealth",
+			"lazy",
+			"mason",
+			"snacks_dashboard",
+			"snacks_notif",
+			"snacks_win",
+		}
 
-          -- let's check again
-          parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+		-- Auto-install parsers and enable highlighting on FileType
+		vim.api.nvim_create_autocmd("FileType", {
+			group = group,
+			desc = "Enable treesitter highlighting and indentation (non-blocking)",
+			callback = function(event)
+				if vim.tbl_contains(ignore_filetypes, event.match) then
+					return
+				end
 
-          if parser_installed then
-            -- Start treesitter for this buffer
-            vim.treesitter.start(bufnr, parser_name)
-          end
-        end,
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = "BufRead",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      event = "BufRead",
-    },
-    opts = {
-      multiwindow = true,
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    branch = "main",
-    keys = {
-      {
-        "af",
-        function()
-          require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
-        end,
-        desc = "Select outer function",
-        mode = { "x", "o" },
-      },
-      {
-        "if",
-        function()
-          require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
-        end,
-        desc = "Select inner function",
-        mode = { "x", "o" },
-      },
-      {
-        "ac",
-        function()
-          require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
-        end,
-        desc = "Select outer class",
-        mode = { "x", "o" },
-      },
-      {
-        "ic",
-        function()
-          require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
-        end,
-        desc = "Select inner class",
-        mode = { "x", "o" },
-      },
-      {
-        "as",
-        function()
-          require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
-        end,
-        desc = "Select local scope",
-        mode = { "x", "o" },
-      },
-    },
-    ---@module "nvim-treesitter-textobjects"
-    opts = { multiwindow = true },
-  },
+				local lang = vim.treesitter.language.get_lang(event.match) or event.match
+				local buf = event.buf
+
+				if parsers_failed[lang] then
+					return
+				end
+
+				if parsers_loaded[lang] then
+					-- Parser already loaded, start immediately (fast path)
+					start(buf, lang)
+				else
+					-- Queue for async loading
+					table.insert(parsers_pending, { buf = buf, lang = lang })
+				end
+
+				-- Auto-install missing parsers (async, no-op if already installed)
+				ts.install({ lang })
+			end,
+		})
+	end,
 }
